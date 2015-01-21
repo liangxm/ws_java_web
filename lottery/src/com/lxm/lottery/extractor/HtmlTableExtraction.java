2837103862690c9d3c72f70ce0c3a278ebcab915
liangxm <liangxm_tom@163.com>
2015-01-21 13:42:41 +0800

    updated the .gitignore file

--- lottery/src/com/lxm/lottery/extractor/HtmlTableExtraction.java
+++ lottery/src/com/lxm/lottery/extractor/HtmlTableExtraction.java
@@ -6,26 +6,53 @@ import java.util.regex.Pattern;
 
 public class HtmlTableExtraction {
 
-	private Matcher tableMacher;
-	private Pattern tablePattern;
+	private Matcher tableMacher,trMatcher1,trMatcher2,trMatcher3;
+	private Pattern tablePattern,trPattern1,trPattern2,trPattern3;
 	
-	private String tableStr;
+	private String tableStr,trStr1,trStr2,trStr3;
 	
-	private static final String HTML_TAG_PATTERN = "(?i)<table class=\"listDataTable1 zebraTable2 mixTable tbNBT\" cellspacing=\"0\" cellpadding=\"0\">(.*?)</table>\\s*</td>\\s*</tr>\\s*</tbody>\\s*</table>";
+	private static final String TABLE_IN_HTML_PATTERN = "(?i)<table class=\"listDataTable1 zebraTable2 mixTable tbNBT\" cellspacing=\"0\" cellpadding=\"0\">(.*?)</table>\\s*</td>\\s*</tr>\\s*</tbody>\\s*</table>";
+	private static final String TR_IN_TABLE_PATTERN1 = "(?is)(<tr playtime=[^>]*>)(.*?)(</tr>)";
+	private static final String TR_IN_TABLE_PATTERN2 = "(?is)(<tr id=\"opt[^>]*>)(.*?)(</tr>)";
+	private static final String TR_IN_TABLE_PATTERN3 = "(?is)(<tr class=\"hide\"[^>]*>)(.*?)(</tr>)";
 	
 	public HtmlTableExtraction(){
-		tablePattern = Pattern.compile(HTML_TAG_PATTERN);
+		tablePattern = Pattern.compile(TABLE_IN_HTML_PATTERN);
+		trPattern1 = Pattern.compile(TR_IN_TABLE_PATTERN1);
+		trPattern2 = Pattern.compile(TR_IN_TABLE_PATTERN2);
+		trPattern3 = Pattern.compile(TR_IN_TABLE_PATTERN3);
 	}
 	
 	public ArrayList<HTMLDivElement> extractHTMLDivs(final String sourceHtml) {
 		ArrayList<HTMLDivElement> elements = new ArrayList<HTMLDivElement>();
 		
 		tableMacher = tablePattern.matcher(sourceHtml);
-		System.out.println("groupCount:"+tableMacher.groupCount());
+		System.out.println("groupCount1:"+tableMacher.groupCount());
 		while (tableMacher.find()) {
 			tableStr=tableMacher.group(0);
 			System.out.println("tableStr:"+tableStr);
+			
+			trMatcher1 = trPattern1.matcher(tableStr);
+			printContent(trMatcher1,"trMatcher1");
+			
+			trMatcher2 = trPattern2.matcher(tableStr);
+			printContent(trMatcher2,"trMatcher2");
+			
+			trMatcher3 = trPattern3.matcher(tableStr);
+			printContent(trMatcher3,"trMatcher3");
 		}
 		return elements;
+	}
+	
+	public void printContent(Matcher matcher,String label){
+		int count = matcher.groupCount();
+		System.out.println(label+"-count:"+count);
+		while(matcher.find()){
+			for(int i=0;i<count;i++){
+				System.out.println(label+"-group-"+i+":"+matcher.group(i));
+			}
+			System.out.println();
+			System.out.println();
+		}
 	}
 }

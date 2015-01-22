f0d302bd74b58bb177476e8b2001901319ec57bb
liangxm <liangxm_tom@163.com>
2015-01-22 08:31:49 +0800

    update some modified files

--- lottery/src/com/lxm/lottery/servlet/WYExtractor.java
+++ lottery/src/com/lxm/lottery/servlet/WYExtractor.java
@@ -1,5 +1,8 @@
 package com.lxm.lottery.servlet;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -12,6 +15,7 @@ import org.json.JSONObject;
 import com.lxm.lottery.html.HttpRequest;
 import com.lxm.lottery.service.MatchService;
 import com.lxm.lottery.service.impl.MatchServiceImpl;
+import com.lxm.lottery.util.StringUtils;
 
 /**
  * Servlet implementation class WYExtractor
@@ -20,15 +24,14 @@ public class WYExtractor extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 	private Timer timer;
-	private static final int delay = 1000*5;
-	private static final int minutes = 1000*60*1;
+	private static final int delay = 1000 * 5;
+	private static final int minutes = 1000 * 60 * 30;
 	private MatchService matchService;
 
 	public void init() throws ServletException {
 		super.init();
 		this.timer = new Timer();
-		this.timer.schedule(new ExtractorService(), delay,
-				minutes);
+		this.timer.schedule(new ExtractorService(), delay, minutes);
 	}
 
 	public void destroy() {
@@ -40,16 +43,33 @@ public class WYExtractor extends HttpServlet {
 
 		public void run() {
 			matchService = new MatchServiceImpl();
-			System.out.println("data extractor runing");
-			String loadData = HttpRequest
-					.sendGet(
-							"http://caipiao.163.com/order/preBet_jclqNewMixAllAjax.html",
-							"");
-			try {
-				JSONObject jo = new JSONObject(loadData);
-				matchService.addMatch(jo);
-			} catch (JSONException e) {
-				e.printStackTrace();
+			Calendar c = Calendar.getInstance();
+			int hour = c.get(Calendar.HOUR_OF_DAY);
+			if (hour != 8) {
+				String loadData = HttpRequest
+						.sendGet(
+								"http://caipiao.163.com/order/preBet_jclqNewMixAllAjax.html",
+								"");
+				try {
+					JSONObject jo = new JSONObject(loadData);
+					matchService.addMatch(jo);
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+			} else {
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+				String loadData = HttpRequest
+						.sendGet(
+								"http://caipiao.163.com/order/preBet_jclqNewMixAllAjax.html",
+								"betDate="
+										+ StringUtils.getSpecifiedDayBefore(sdf
+												.format(new Date())));
+				try {
+					JSONObject jo = new JSONObject(loadData);
+					matchService.addMatch(jo);
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 	}
--- lottery/src/com/lxm/lottery/util/StringUtils.java
+++ lottery/src/com/lxm/lottery/util/StringUtils.java
@@ -1,5 +1,10 @@
 package com.lxm.lottery.util;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class StringUtils {
 
 	public static boolean isInteger(String value) {
@@ -9,5 +14,37 @@ public class StringUtils {
 		} catch (NumberFormatException e) {
 			return false;
 		}
+	}
+
+	/**
+	 * 获得指定日期的前一天
+	 * 
+	 * @param specifiedDay
+	 * @return
+	 * @throws Exception
+	 */
+	public static String getSpecifiedDayBefore(String specifiedDay) {
+		// SimpleDateFormat simpleDateFormat = new
+		// SimpleDateFormat("yyyy-MM-dd");
+		Calendar c = Calendar.getInstance();
+		Date date = null;
+		try {
+			date = new SimpleDateFormat("yyyyMMdd").parse(specifiedDay);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		c.setTime(date);
+		int day = c.get(Calendar.DATE);
+		c.set(Calendar.DATE, day - 1);
+
+		String dayBefore = new SimpleDateFormat("yyyyMMdd").format(c
+				.getTime());
+		return dayBefore;
+	}
+
+	public static void main(String[] args) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+		
+		System.out.println(getSpecifiedDayBefore(sdf.format(new Date())));
 	}
 }
